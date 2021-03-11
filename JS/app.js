4d12e08226d35b4f47e7596cfab652c7ad02b598
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-03-11 16:10:42 +0200

    save

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+const name = 'Roman';
+const age = 38;
+
+
+
+console.log(`Мое имя ${name} мой возраст ${age}`);
+
+const person = ['Роман', 'Михаил', 'Тимур', 'Денис'];
+
+
+
+
+person[0] = `Иван`;
+console.log(person.length);
+console.log(person);
