a66903a2a33651bf2d5339f58061447b6855a429
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-03-12 16:06:56 +0200

    save

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,46 @@
-const name = 'Roman';
-const age = 38;
+// const name = 'Roman';
+// const age = 38;
 
 
 
-console.log(`Мое имя ${name} мой возраст ${age}`);
+// console.log(`Мое имя ${name} мой возраст ${age}`);
 
-const person = ['Роман', 'Михаил', 'Тимур', 'Денис'];
-
-
+// const person = ['Роман', 'Михаил', 'Тимур', 'Денис'];
 
 
-person[0] = `Иван`;
-console.log(person.length);
-console.log(person);
+
+
+// person[0] = `Иван`;
+// console.log(person.length);
+// console.log(person);
+
+// let shou = confirm(`Продливаете подписку?`);
+
+// console.log(shou);
+
+// let quantity = prompt(`Введите количество товаров`);
+// quantity = Number(quantity);
+// console.log(quantity);
+
+
+
+// parseInt  выводич число и останавливается на первом знакуе которое не число----------------------------------
+// let element = `50px`;
+
+// const result = Number.parseInt(element);
+
+// console.log(result);
+
+// let test = `154мм`;
+
+// const res = Number.parseInt(test);
+
+// console.log(res);
+
+// toFixed   убтрает цыфры после запятой и оставляет то колличество которое указано в ( )
+
+let salary = 30000.1254;
+salary = Number(salary.toFixed(5));
+
+
+console.log(salary);
