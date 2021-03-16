a7b75e05740daae50dc49978d77c9be12e04aaff
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-03-16 11:45:59 +0200

    save

--- JS/app.js
+++ JS/app.js
@@ -45,13 +45,33 @@
 
 // console.log(salary);
 
-let base = prompt(`Введи сюда число `);
-base = Number(base);
-console.log(base);
+// let base = prompt(`Введи сюда число `);
+// base = Number(base);
+// console.log(base);
 
-let power = prompt(`Введи сюда степень`);
-power = Number(power);
-console.log(power);
+// let power = prompt(`Введи сюда степень`);
+// power = Number(power);
+// console.log(power);
 
-const result = base ** power;
-console.log(result);
+// const result = base ** power;
+// console.log(result);
+ 
+// Начинаем выполнять код по условия спомощью if  Логическое ветление
+// if (5 > 100) {
+//     console.log('Первый код');
+// }
+//  else {
+//     console.log('Второй код если в условии if стоит false');    
+//  }
+
+// const salary = prompt('Введи сюда свою зарплату');
+
+// if (salary <= 500) {
+//     console.log('Уровень зарплаты 1');
+// }
+// else if (salary > 500 && salary < 1500) {
+//     console.log('Уровень зарплаты 2');
+// }
+// else {
+//    console.log('Уровень зарплаты 3') 
+// };
