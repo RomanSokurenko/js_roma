6a01aec7141bc638fd1aa230c47334ccb875f1b7
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-03-16 13:35:27 +0200

    save

---
+++ JS/app2.js
@@ -0,0 +1,65 @@
+// const stars = 8;
+// let price;
+
+// switch (stars) {
+//     case 1:
+//         price = 20;
+//         break;
+//     case 2:
+//         price = 40;
+//         break;
+//     case 3:
+//         price = 50;
+//         break;
+//     case 4:
+//         price = 80;
+//         break;
+//     case 5:
+//         price = 150;
+//     default: 
+//     console.log ('Такого количества звезд нет!!!');
+        
+
+// }
+// console.log(price);
+
+// const stars = 6;
+// let price;
+
+// switch (stars) {
+//     case 1:
+//         case 2:
+//         price = 30;
+//         break;
+//     case 3:
+//         case 4:
+//         price = 80;
+//         break;
+//     case 5:
+//         price = 150;
+//         break;
+
+//     default: 
+//     console.log ('Такого количества звезд нет!!!');
+        
+
+// }
+// console.log(price);
+
+// const option = 7;
+// let message = '';
+
+// switch (option) {
+//     case 1:
+//         message = "Ваш товар доставят завтра";
+//         break;
+//     case 2:
+//         message = 'Заберите на отделении завтра';
+//         break;
+//     case 3:
+//         message = 'товар отсутствует';
+//         break;
+//     default: console.log('С Вами свяжится оператор');
+        
+// }
+// console.log(message);
