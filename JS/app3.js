4a35580ec05f96a1d1dc60cb85ba25f13f0c01d8
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-03-17 16:15:07 +0200

    save

--- JS/app3.js
+++ JS/app3.js
@@ -1,9 +1,32 @@
 // МАСИВЫ-------------------------------------
-const cars = ['a', 's', 'd'];
+// const cars = ['a', 's', 'd'];
 // узнать длинну
-console.log(cars.length - 1);
+// console.log(cars.length - 1);
 // вызвать в масиве любой елемент
-console.log(cars[1]);
+// console.log(cars[1]);
 // заменить в новом масиве любой элемент
-cars[0] = 'qwer';
-console.log(cars);
+// cars[0] = 'qwer';
+// console.log(cars);
+
+// Примитивы и сложные типы 
+// let a = 100;
+// let b = a;
+// console.log(a);
+// console.log(b);
+
+// a = 50;
+
+// console.log(a);
+// console.log(b);
+
+// let a = [1, 2, 3];
+// let b = a;
+// console.log(a);
+// console.log(b);
+// a[0] = 357;
+// console.log(a);
+// console.log(b);
+
+// ПЕРЕБОР МАСИВА---------------------------------------------
+const letters = ['a', 's', 'd', 'g'];
+console.log(letters);
