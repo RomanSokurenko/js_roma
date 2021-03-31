95cba1324db5bc8cd60b62b718ae7d099865f08c
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-03-31 16:15:44 +0300

    123

--- JS/newjs.js
+++ JS/newjs.js
@@ -1,1 +1,15 @@
-console.log('Hello Roman');
+// console.log('Hello Roman', "new",357);
+// let a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+// a = 'Cars 42';
+// console.log(a);
+// const message = "Привет как дела???"
+// console.log(message.length);
+// console.log(message.slice(8));
+// console.log('jhgfd' > 5);
+if (5) {
+    console.log('если да')
+}
+else { console.log('если нет') };
