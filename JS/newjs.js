6850e7a89d7b58ab9db44b5e931ea2261dc26b3f
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-04-01 00:41:06 +0300

    save

--- JS/newjs.js
+++ JS/newjs.js
@@ -9,7 +9,36 @@
 // console.log(message.length);
 // console.log(message.slice(8));
 // console.log('jhgfd' > 5);
-if (5) {
-    console.log('если да')
+// if (5) {
+//     console.log('если да')
+// }
+// else { console.log('если нет') };
+
+// for (let i = 0; i <= 5; i += 1){
+//     console.log(i);
+// }
+
+// const min = 1;
+// const max = 10;
+// let total = 0;
+
+// for (let i = min; i <=max; i += 1){
+//     console.log(i);
+//     if (i % 2 !== 0) {
+//         console.log('данное число не четное', i);
+//         continue;
+// }
+//     console.log('данное число четное ', i);
+//     total += i;
+// }
+// console.log('total',total);
+
+// const age = 2;
+// const type = age >= 18 ? 'adult' : 'child';
+// console.log(type);
+
+const max = 10;
+
+for (let i = 0; i < max; i += 1) {
+  console.log(`${max} % ${i} = `, max % i);
 }
-else { console.log('если нет') };
